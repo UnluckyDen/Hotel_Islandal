617b3378e4c1387227fda0d5e0e3d94071b1e5d9
UnluckyDen <dsgladkih01@gmail.com>
2025-04-17 08:44:03 +0300

    add trashable

--- Assets/_Main/Scripts/Interfaces/IMovableObject.cs
+++ Assets/_Main/Scripts/Interfaces/IMovableObject.cs
@@ -2,6 +2,7 @@ namespace _Main.Scripts.Interfaces
 {
     public interface IMovableObject : IHoverable, ITransformContains
     {
+        public bool IsTrashable { get; }
         public void ToNonInteractive();
         public void ToInteractable();
     }
